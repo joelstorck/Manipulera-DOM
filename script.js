fafadde3aa21ad7fd31d7c0e5c5affd60f17f38b
Joel Storck <stjo20zs@student.ju.se>
2020-09-01 17:07:57 +0200

    Lagt till namn + Array

---
+++ script.js
@@ -0,0 +1,17 @@
+var firstDiv = document.createElement("div");
+    var para = document.createElement("p");
+        para.innerText = "Joel Storck";
+        firstDiv.appendChild(para) 
+
+
+var secondDiv = document.createElement("div");
+    var goodThings = ["Sova", " Jobba", " Äta"," Leka"];
+    var attributes = document.createElement("p");
+        attributes.innerText = (goodThings);
+        secondDiv.appendChild(attributes);
+
+            document.body.appendChild(firstDiv);
+            document.body.appendChild(secondDiv);
+
+
+
